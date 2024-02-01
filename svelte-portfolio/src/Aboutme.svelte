bbecfdf873b462fba860b1e31cfccf6abbe54428
Ong Riverine <setsubi9@gmail.com>
2024-02-01 12:40:54 +0700

    .

--- svelte-portfolio/src/Aboutme.svelte
+++ svelte-portfolio/src/Aboutme.svelte
@@ -7,9 +7,9 @@
 
       <p>Hiya my name is Ong Riverine 👋, I’m an undergraduate student at Sanata Dharma University, Bangkit 2023 Batch 1 Cloud Computing participant, Bangkit 2023 Batch 2 Mobile Dev Participant <b>(graduated with distinction)</b>.
         I am a lover of tech, tea and all its wonders. I marvel at the power of cloud computing, Back-end dev, UI/UX and the infinite possibilities it offers. I delight in learning new things and expanding my horizons. I cherish writing and art as forms of expression and creativity...</p>
-        <img alt="YouTube Channel Subscribers" src="https://img.shields.io/youtube/channel/subscribers/UC5s3FdG5mlgcQ9XofSF8p6w?style=for-the-badge&logo=Youtube&logoColor=red">
-        <img alt="Linked In" src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white">
-        <img alt="Github" src="https://img.shields.io/badge/Github-000000?style=for-the-badge&logo=github&logoColor=white">
+        <a href="https://www.youtube.com/channel/UC5s3FdG5mlgcQ9XofSF8p6w"><img alt="YouTube Channel Subscribers" src="https://img.shields.io/youtube/channel/subscribers/UC5s3FdG5mlgcQ9XofSF8p6w?style=for-the-badge&logo=Youtube&logoColor=red"></a>
+        <a href="https://www.linkedin.com/in/ong-gabriel-riverine-susanto-8994b2266/"><img alt="Linked In" src="https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white"></a>
+        <a href="https://github.com/Riveong"><img alt="Github" src="https://img.shields.io/badge/Github-000000?style=for-the-badge&logo=github&logoColor=white"></a>
       
     
         <h2>Interest</h2>
